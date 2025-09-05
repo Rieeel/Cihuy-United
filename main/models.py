0bebe2757ffdb0507c1aacb986d603d20a96b747
Rieeel <ldepuuwu14@gmail.com>
2025-09-05 14:31:08 +0700

    .

--- main/models.py
+++ main/models.py
@@ -11,6 +11,9 @@ class Product(models.Model):
     is_featured = models.BooleanField(default=False)
     
     def __str__(self):
+        return self.name
+    
+    def __str__(self):
         return self.title
     
     @property
--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ def show_main(request):
     context = {
         'nama_aplikasi' : 'Cihuy United',
         'name': 'Muhammad Derriel Ramadhan',
-        'class': 'PBP F'
+        'class': 'PBP F',
     }
     
     return render(request, "main.html", context)
