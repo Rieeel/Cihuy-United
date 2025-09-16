e9927f0dca272688d9468a256b63f214a0ff595f
Rieeel <ldepuuwu14@gmail.com>
2025-09-16 16:40:57 +0700

    Tugas 3

--- Cihuy_United/settings.py
+++ Cihuy_United/settings.py
@@ -32,6 +32,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "muhammad-derriel-cihuyunited.pbp.cs.ui.ac.id"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://muhammad-derriel-cihuyunited.pbp.cs.ui.ac.id",
+]
+
 
 # Application definition
 
